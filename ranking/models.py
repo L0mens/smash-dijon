3e410318be8550bf0450650b0319bbff0aea2c0f
l0mens <lomens@hotmail.fr>
2019-10-30 17:37:05 +0100

    remove credential model

--- ranking/models.py
+++ ranking/models.py
@@ -126,10 +126,3 @@ class Vodplaylist(models.Model):
     def __str__(self):
         return f"{self.youtube_id} {self.name}"
 
-# class CredentialsYoutube(models.Model):
-#     token = models.CharField(max_length=255) 
-#     refresh_token = models.CharField(max_length=255)
-#     token_uri = models.CharField(max_length=255)
-#     client_id = models.CharField(max_length=255)
-#     client_secret = models.CharField(max_length=255) 
-#     scopes = models.CharField(max_length=255) 
