733500f0ef384de207ee1ca8393195b113181ec4
l0mens <lomens@hotmail.fr>
2019-12-20 12:31:58 +0100

    Fix worst enemie

--- infos/statistics.py
+++ infos/statistics.py
@@ -3,10 +3,12 @@ def get_worst_enemie(matches_loose):
     list_win = []
     for match_l in matches_loose:
         list_win.append(match_l.winner.competitor.name)
-
+    print(list_win)
     nb_win_max = 0
     worst_enemie = ""
     for player,nb_win in Counter(list_win).items():
         if nb_win_max < nb_win:
             worst_enemie = player
+            nb_win_max = nb_win
+    
     return worst_enemie
