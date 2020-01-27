bf1efb428096ca7f63e42f2324eb1c45281c39a4
l0mens <lomens@hotmail.fr>
2020-01-27 23:31:08 +0100

    Confort update on player list Worst EN

--- infos/statistics.py
+++ infos/statistics.py
@@ -5,10 +5,18 @@ def get_worst_enemie(matches_loose):
         list_win.append(match_l.winner.competitor.name)
 
     nb_win_max = 0
-    worst_enemie = ""
+    worst_enemie = []
     for player,nb_win in Counter(list_win).items():
         if nb_win_max < nb_win:
-            worst_enemie = player
+            worst_enemie = [player]
             nb_win_max = nb_win
+        elif nb_win_max == nb_win:
+            worst_enemie.append(player)
     
-    return worst_enemie
+    return ", ".join(worst_enemie)
+
+
+class Statistics():
+
+    def __init__(self, list_matches):
+        self.matches = list_matches
